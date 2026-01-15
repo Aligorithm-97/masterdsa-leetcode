7bfffab0a9636fdaae866e39aa1c0017f757033b
Aligorithm-97 <atemurtas97@gmail.com>
2026-01-15 19:36:51 +0300

    aligorithm-validpalindrome

--- twopointers/leetcode125_validpalindrome/ValidPalindrome.java
+++ twopointers/leetcode125_validpalindrome/ValidPalindrome.java
@@ -12,10 +12,32 @@ public class ValidPalindrome {
         return sb.toString().equals(sb.reverse().toString());
     }
 
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
+    }
 
-    public static void main(String[] args) {
-
+    public boolean isPalindromeTwoPointers(String s) {
+        int left = 0, right = s.length() - 1;
+        while(left < right){
+            while(left < right && !alphaNum(s.charAt(left))){
+                left++;
+            }
+            while(right > left && !alphaNum(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+        public static void main(String[] args) {
         ValidPalindrome solution = new ValidPalindrome();
         System.out.println(solution.isPalindromeReverse("Was it a car or a cat I saw?"));
+        System.out.println(solution.isPalindromeTwoPointers("Was it a car or a cat I saw?"));
     }
 }
