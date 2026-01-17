5992d905c681af546026a4f5f67de4f901d15c68
Aligorithm-97 <atemurtas97@gmail.com>
2026-01-17 16:10:43 +0300

    aligorithm-validparentheses

--- stack/leetcode20_validparentheses/ValidParentheses.java
+++ stack/leetcode20_validparentheses/ValidParentheses.java
@@ -1,8 +1,39 @@
 package stack.leetcode20_validparentheses;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class ValidParentheses {
 
+    public boolean isValidStack(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> closeToOpen = new HashMap<>();
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+
+
     public static void main(String[] args) {
+
+        ValidParentheses solution = new ValidParentheses();
+        System.out.println(solution.isValidStack("([)]"));
 
     }
 }
