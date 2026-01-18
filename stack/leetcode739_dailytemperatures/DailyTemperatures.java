a71ca35858576614d60f4ebacf0444fc3fed11e4
Aligorithm-97 <atemurtas97@gmail.com>
2026-01-18 18:25:45 +0300

    aligorithm-dailytemperatures

--- stack/leetcode739_dailytemperatures/DailyTemperatures.java
+++ stack/leetcode739_dailytemperatures/DailyTemperatures.java
@@ -1,8 +1,27 @@
 package stack.leetcode739_dailytemperatures;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class DailyTemperatures {
 
-    public static void main(String[] args) {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>();
 
+        for (int i = 0; i < temperatures.length; i++) {
+            int t = temperatures[i];
+            while (!stack.isEmpty() && t > stack.peek()[0]) {
+                int[] pair = stack.pop();
+                res[pair[1]] = i - pair[1];
+            }
+            stack.push(new int[]{t, i});
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        DailyTemperatures dt = new DailyTemperatures();
+        System.out.println(Arrays.toString(dt.dailyTemperatures(new int[]{30,38,30,36,35,40,28})));
     }
 }
