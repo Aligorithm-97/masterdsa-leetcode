66c38d75511d8e72f5d2431b93f172eb32396452
Aligorithm-97 <atemurtas97@gmail.com>
2026-01-19 20:24:10 +0300

    aligorithm-binarysearch

--- binarysearch/leetcode704_binarysearch/BinarySearch.java
+++ binarysearch/leetcode704_binarysearch/BinarySearch.java
@@ -1,8 +1,25 @@
 package binarysearch.leetcode704_binarysearch;
 
 public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int l=0, r=nums.length-1;
+
+        while (l<=r){
+            int mid = l+((r-l)/2);
+            if(nums[mid]>target){
+                r=mid-1;
+            } else if(nums[mid]<target){
+                l=mid+1;
+            }  else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
+        BinarySearch bs = new BinarySearch();
 
+        System.out.println(bs.search(new int[]{-1,0,2,4,6,8}, 4));
     }
 }
