22e31c252a6df7c593332cd7120673236a29b5b1
Aligorithm-97 <atemurtas97@gmail.com>
2026-01-20 19:38:05 +0300

    aligorithm-kokoeatingbananas

--- binarysearch/leetcode875_kokoeatingbananas/KokoEatingBananas.java
+++ binarysearch/leetcode875_kokoeatingbananas/KokoEatingBananas.java
@@ -1,8 +1,32 @@
 package binarysearch.leetcode875_kokoeatingbananas;
 
+import java.util.Arrays;
+
 public class KokoEatingBananas {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = Arrays.stream(piles).max().getAsInt();
+        int res = r;
+
+        while (l <= r) {
+            int k = (l + r) / 2;
+
+            long totalTime = 0;
+            for (int p : piles) {
+                totalTime += Math.ceil((double) p / k);
+            }
+            if (totalTime <= h) {
+                res = k;
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
-
+        KokoEatingBananas koko = new KokoEatingBananas();
+        System.out.println(koko.minEatingSpeed(new int[] {1,4,3,2}, 9));
     }
 }
