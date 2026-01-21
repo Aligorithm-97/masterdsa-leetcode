213edc2395b4029b83b86444d4113bf93607b70a
Aligorithm-97 <atemurtas97@gmail.com>
2026-01-21 20:00:11 +0300

    aligorithm-besttimetobuyandsellstock

--- slidingwindow/leetcode121_besttimetobuyandsellstock/BestTimeToBuyAndSellStock.java
+++ slidingwindow/leetcode121_besttimetobuyandsellstock/BestTimeToBuyAndSellStock.java
@@ -2,7 +2,24 @@ package slidingwindow.leetcode121_besttimetobuyandsellstock;
 
 public class BestTimeToBuyAndSellStock {
 
-    public static void main(String[] args) {
+    public int maxProfitTwoPointers(int[] prices) {
+        int l = 0, r = 1;
+        int maxP = 0;
 
+        while (r < prices.length) {
+            if (prices[l] < prices[r]) {
+                int profit = prices[r] - prices[l];
+                maxP = Math.max(maxP, profit);
+            } else {
+                l = r;
+            }
+            r++;
+        }
+        return maxP;
+    }
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock bt = new BestTimeToBuyAndSellStock();
+        System.out.println(bt.maxProfitTwoPointers(new int[]{10,1,5,6,7,1}));
     }
 }
