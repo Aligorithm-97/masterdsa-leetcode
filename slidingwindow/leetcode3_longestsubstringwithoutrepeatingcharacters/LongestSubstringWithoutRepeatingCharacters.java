78ecfa19efdf102357375a132aeb7d79c13a17e8
Aligorithm-97 <atemurtas97@gmail.com>
2026-01-22 19:18:51 +0300

    aligorithm-longestsubstringwithoutrepeatingcharacters

--- slidingwindow/leetcode3_longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ slidingwindow/leetcode3_longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,6 @@
 package slidingwindow.leetcode3_longestsubstringwithoutrepeatingcharacters;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters {
@@ -20,10 +21,25 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return res;
     }
 
+    public int lengthOfLongestSubstringSlidingWindowOptimal(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        int l = 0, res = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            if (mp.containsKey(s.charAt(r))) {
+                l = Math.max(mp.get(s.charAt(r))+1, l);
+            }
+            mp.put(s.charAt(r), r);
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+
 
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters sol = new LongestSubstringWithoutRepeatingCharacters();
         System.out.println(sol.lengthOfLongestSubstringSlidingWindow("zxyzxyz"));
+        System.out.println(sol.lengthOfLongestSubstringSlidingWindowOptimal("zxyzxyz"));
     }
 
 }
