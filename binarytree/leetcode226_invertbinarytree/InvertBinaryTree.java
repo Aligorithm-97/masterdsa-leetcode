d9ef046b354f7dead4ec71fb468d0f90e19eccff
Aligorithm-97 <atemurtas97@gmail.com>
2026-02-02 21:55:14 +0300

    aligorithm-invertbinarytree

--- binarytree/leetcode226_invertbinarytree/InvertBinaryTree.java
+++ binarytree/leetcode226_invertbinarytree/InvertBinaryTree.java
@@ -1,7 +1,30 @@
 package binarytree.leetcode226_invertbinarytree;
+import java.util.LinkedList;
+import java.util.Queue;
 
+import static binarytree.BinaryTreeUtils.TreeNode;
 public class InvertBinaryTree {
-    public static void main(String[] args) {
 
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) {}
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
     }
 }
